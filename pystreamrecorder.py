a1c1eb2b7c6603c9184860540c6ff1d394f47b60
walkofpain <87929569+walkofpain@users.noreply.github.com>
2021-07-25 14:10:44 +0500

    Add files via upload

---
+++ pystreamrecorder.py
@@ -0,0 +1,28 @@
+import schedule
+import datetime
+import time
+import subprocess
+
+URL = 'https://www.youtube.com/channel/UCk73U4QT3cNDvqb_PaWM8AA/live' # stream channel link (youtube/twitch/wasd), youtube link with "/live"
+quality = 'best' # choose quality (audio_only, worst, best)
+
+text = '''
+▒█▀▀█ █░░█ ▒█▀▀▀█ ▀▀█▀▀ █▀▀█ █▀▀ █▀▀█ █▀▄▀█ ▒█▀▀█ █▀▀ █▀▀ █▀▀█ █▀▀█ █▀▀▄ █▀▀ █▀▀█ 
+▒█▄▄█ █▄▄█ ░▀▀▀▄▄ ░░█░░ █▄▄▀ █▀▀ █▄▄█ █░▀░█ ▒█▄▄▀ █▀▀ █░░ █░░█ █▄▄▀ █░░█ █▀▀ █▄▄▀ 
+▒█░░░ ▄▄▄█ ▒█▄▄▄█ ░░▀░░ ▀░▀▀ ▀▀▀ ▀░░▀ ▀░░░▀ ▒█░▒█ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░▀▀ ▀▀▀░ ▀▀▀ ▀░▀▀
+                                                                   <3 @walkofpain
+'''
+print(text)
+
+def job_function():
+    now = datetime.datetime.now()
+    outputfile = 'stream-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'-'+str(now.hour)+'-'+str(now.minute)+'.mp4'
+    
+    command='streamlink ' + URL + ' '+ quality + ' -o '  + outputfile
+    subprocess.call(command, shell=True)
+
+schedule.every(2).seconds.do(job_function) 
+
+while True:
+    schedule.run_pending()
+    time.sleep(0)
